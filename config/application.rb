a974ddd3fcce5acf16261bc6a46e589bc0e03830
Stephen McGinty <stephen.f.mcginty@gmail.com>
2017-05-24 11:30:25 +0100

    Complete overhaul of the methodology

--- config/application.rb
+++ config/application.rb
@@ -1,94 +1,101 @@
 require 'origen'
 class OrigenUpdaterApplication < Origen::Application
-
   # See http://origen-sdk.org/origen/api/Origen/Application/Configuration.html
   # for a full list of the configuration options available
 
   # These attributes should never be changed, the duplication here will be resolved in future
   # by condensing these attributes that do similar things
-  self.name       = "origen_updater"
-  self.namespace  = "OrigenUpdater"
-  config.name     = "origen_updater"
-  config.initials = "OrigenUpdater"
-  config.rc_url   = "git@github.com:Origen-SDK/origen_updater.git"
+  self.name       = 'origen_updater'
+  self.namespace  = 'OrigenUpdater'
+  config.name     = 'origen_updater'
+  config.initials = 'OrigenUpdater'
+  config.rc_url   = 'git@github.com:Origen-SDK/origen_updater.git'
   config.release_externally = true
 
   # To enable deployment of your documentation to a web server (via the 'origen web'
   # command) fill in these attributes.
-  #config.web_directory = "git@github.com:Origen-SDK/Origen-SDK.github.io.git/origen_updater"
-  #config.web_domain = "http://origen-sdk.org/origen_updater"
+  # config.web_directory = "git@github.com:Origen-SDK/Origen-SDK.github.io.git/origen_updater"
+  # config.web_domain = "http://origen-sdk.org/origen_updater"
 
   # When false Origen will be less strict about checking for some common coding errors,
   # it is recommended that you leave this to true for better feedback and easier debug.
   # This will be the default setting in Origen v3.
   config.strict_errors = true
 
-  config.shared = {
-    command_launcher: "config/shared_commands.rb"
-  }
-
   # See: http://origen-sdk.org/origen/latest/guides/utilities/lint/
   config.lint_test = {
     # Require the lint tests to pass before allowing a release to proceed
-    run_on_tag: true,
+    run_on_tag:   true,
     # Auto correct violations where possible whenever 'origen lint' is run
-    auto_correct: true, 
+    auto_correct: true,
     # Limit the testing for large legacy applications
-    #level: :easy,
+    # level: :easy,
     # Run on these directories/files by default
-    #files: ["lib", "config/application.rb"],
+    files:        ['lib', 'config/application.rb', 'bin']
   }
 
   config.semantically_version = true
 
+  def before_release_tag(identifier, note, type, selector, options)
+    v = Origen::VersionString.new(identifier)
+    # Update the version in the command files
+    f = "#{Origen.root}/bin/fix_my_workspace"
+    data = File.read(f)
+    filtered_data = data.sub(/\$_fix_my_workspace_version = '\d+\.\d+\.\d+'/, "$_fix_my_workspace_version = '#{v}'")
+    File.open(f, 'w') do |f|
+      f.write(filtered_data)
+    end
+    Origen.app.rc.checkin f, comment: 'Wrote new version in command code'
+  end
+
   # An example of how to set application specific LSF parameters
-  #config.lsf.project = "msg.te"
-  
+  # config.lsf.project = "msg.te"
+
   # An example of how to specify a prefix to add to all generated patterns
-  #config.pattern_prefix = "nvm"
+  # config.pattern_prefix = "nvm"
 
   # An example of how to add header comments to all generated patterns
-  #config.pattern_header do
+  # config.pattern_header do
   #  cc "This is a pattern created by the example origen application"
-  #end
+  # end
 
   # By default all generated output will end up in ./output.
   # Here you can specify an alternative directory entirely, or make it dynamic such that
-  # the output ends up in a setup specific directory. 
-  #config.output_directory do
+  # the output ends up in a setup specific directory.
+  # config.output_directory do
   #  "#{Origen.root}/output/#{$dut.class}"
-  #end
+  # end
 
   # Similarly for the reference files, generally you want to setup the reference directory
   # structure to mirror that of your output directory structure.
-  #config.reference_directory do
+  # config.reference_directory do
   #  "#{Origen.root}/.ref/#{$dut.class}"
-  #end
- 
+  # end
+
   # This will automatically deploy your documentation after every tag
-  #def after_release_email(tag, note, type, selector, options)
+  # def after_release_email(tag, note, type, selector, options)
   #  command = "origen web compile --remote --api"
   #  Dir.chdir Origen.root do
   #    system command
   #  end
-  #end
+  # end
 
   # Ensure that all tests pass before allowing a release to continue
-  #def validate_release
+  # def validate_release
   #  if !system("origen specs") || !system("origen examples")
   #    puts "Sorry but you can't release with failing tests, please fix them and try again."
   #    exit 1
   #  else
   #    puts "All tests passing, proceeding with release process!"
   #  end
-  #end
+  # end
 
   # To enabled source-less pattern generation create a class (for example PatternDispatcher)
   # to generate the pattern. This should return false if the requested pattern has been
   # dispatched, otherwise Origen will proceed with looking up a pattern source as normal.
-  #def before_pattern_lookup(requested_pattern)
+  # def before_pattern_lookup(requested_pattern)
   #  PatternDispatcher.new.dispatch_or_return(requested_pattern)
-  #end
+  # end
 
   # If you use pattern iterators you may come across the case where you request a pattern
   # like this:
@@ -96,11 +103,10 @@ class OrigenUpdaterApplication < Origen::Application
   #
   # However it cannot be found by Origen since the pattern name is actually example_pat_bx.atp
   # In the case where the pattern cannot be found Origen will pass the name to this translator
-  # if it exists, and here you can make any substitutions to help Origen find the file you 
+  # if it exists, and here you can make any substitutions to help Origen find the file you
   # want. In this example any instances of _b\d, where \d means a number, are replaced by
   # _bx.
-  #config.pattern_name_translator do |name|
+  # config.pattern_name_translator do |name|
   #  name.gsub(/_b\d/, "_bx")
-  #end
-
+  # end
 end
--- lib/origen_updater.rb
+++ lib/origen_updater.rb
@@ -1,6 +1,18 @@
 require 'origen'
 require_relative '../config/application.rb'
-module OrigenUpdater
-  # Don't load anything from here, everything must load from the update command and with
-  # no dependencies
+
+# Ensure the app always has the latest version of the updater scripts
+app_fix_my_workspace = "#{Origen.root}/bin/fix_my_workspace"
+if File.exist?(app_fix_my_workspace)
+  $_fix_my_workspace_version_check = true
+  load app_fix_my_workspace
+end
+
+if $_fix_my_workspace_version != Origen.app!.version
+  master_fix_my_workspace = File.expand_path('../../bin/fix_my_workspace', __FILE__)
+  FileUtils.mkdir_p "#{Origen.root}/bin" unless File.exist?("#{Origen.root}/bin")
+  # When running standalone
+  unless master_fix_my_workspace == app_fix_my_workspace
+    FileUtils.cp master_fix_my_workspace, app_fix_my_workspace
+  end
 end
